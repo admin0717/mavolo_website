fc35ad22a2ee58c968148b8655423d96d8ad59e5
Barun Debnath <barundebnath91@gmail.com>
2022-09-02 18:31:21 +0530

    hero text size changed

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -18,22 +18,22 @@ const Hero: FC = () => {
           <div className="text-center mx-auto lg:mx-0">
             <Image src={LineRounded} alt="Rounded Line" />
           </div>
-          <div className="font-Inter font-bold lg:text-2xl md:text-3xl text-2xl tracking-wide leading-tight text-center mx-auto px-auto lg:mx-0 lg:px-0">
+          <div className="font-Inter font-bold lg:text-2xl md:text-3xl text-3xl tracking-wide leading-tight text-center mx-auto px-auto lg:mx-0 lg:px-0">
             Say YES to clutter-free style of living.
           </div>
-          <div className="font-Inter font-large  text-sm md:text-md lg:text-lg  leading-6 text-center tracking-wide lg:text-start">
+          <div className="font-Inter font-large  text-lg md:text-md lg:text-lg  leading-6 text-center tracking-wide lg:text-start">
             Experience seamless charging and overcome the hassle of multiple
             chargers with the single point integrated charging solution which
             comes with compatible, compact and fast charging design.
           </div>
           <div className="flex justify-center lg:items-start items-center px-auto mx-auto space-x-8 lg:mx-0 lg:px-0">
             <Link href="/">
-              <a className="font-Inter font-normal text-xl md:text-lg   cursor-pointer p-2 rounded-md hover:border-2 hover:bg-primaryBG drop-shadow-md hover:text-buttonBG bg-buttonBG text-buttonText px-4">
+              <a className="font-Inter font-normal text-2xl md:text-lg   cursor-pointer p-2 rounded-md hover:border-2 hover:bg-primaryBG drop-shadow-md hover:text-buttonBG bg-buttonBG text-buttonText px-4">
                 Explore
               </a>
             </Link>
             <Link href="#inquire">
-              <a className="font-Inter font-normal text-xl  lg:text-lg   cursor-pointer p-2 rounded-md hover:border-2 hover:bg-primaryBG drop-shadow-md hover:text-buttonBG bg-buttonBG text-buttonText px-4">
+              <a className="font-Inter font-normal text-2xl  lg:text-lg   cursor-pointer p-2 rounded-md hover:border-2 hover:bg-primaryBG drop-shadow-md hover:text-buttonBG bg-buttonBG text-buttonText px-4">
                 Inquire
               </a>
             </Link>
